671fa40be5ea905a0390ed6b87422c0d389f6a08
IC-faycal <faycal@inovea-conseil.com>
2022-07-13 14:11:49 +0200

    first commit

---
+++ src/main/java/com/ecovoit/EcoVoitAPI/EcoVoitApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecovoit.EcoVoitAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoVoitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoVoitApiApplication.class, args);
+	}
+
+}
